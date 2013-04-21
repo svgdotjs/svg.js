b2a814bb226978966b02d22b405a895f5abf0818
Ben Scholzen <mail@dasprids.de>
2013-04-21 16:33:57 +0200

    Add sub-pixel offset handling

--- src/doc.js
+++ src/doc.js
@@ -16,7 +16,7 @@ SVG.Doc = function(element) {
     .attr('xlink', SVG.xlink, SVG.ns)
     .defs()
   
-  /* ensure correct rendering for safari */
+  /* ensure correct rendering */
   if (this.parent.nodeName != 'svg')
     this.stage()
 }
@@ -27,6 +27,7 @@ SVG.Doc.prototype = new SVG.Container
 // Hack for safari preventing text to be rendered in one line.
 // Basically it sets the position of the svg node to absolute
 // when the dom is loaded, and resets it to relative a few milliseconds later.
+// Also handles sub-pixel offset rendering properly.
 SVG.Doc.prototype.stage = function() {
   var check
     , element = this
@@ -52,6 +53,9 @@ SVG.Doc.prototype.stage = function() {
         element.node.parentNode.removeChild(element.node)
         element.parent.appendChild(element.node)
         
+        /* after wrapping is done, fix sub-pixel offset */
+        element.fixSubPixelOffset();
+        window.addEventListener('resize', element.fixSubPixelOffset)
       }, 5)
     } else {
       setTimeout(check, 10)
@@ -61,4 +65,14 @@ SVG.Doc.prototype.stage = function() {
   check()
   
   return this
+}
+
+// Fix for possible sub-pixel offset. See:
+// https://bugzilla.mozilla.org/show_bug.cgi?id=608812
+SVG.Doc.prototype.fixSubPixelOffset = function() {
+  var pos  = this.node.getScreenCTM();
+  var left = -pos.e % 1;
+  var top  = -pos.f % 1;
+
+  this.node.style('left: ' + left + 'px;top:' + top + 'px;');
 }
