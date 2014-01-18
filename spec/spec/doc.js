08a750a6d8726a47c5c35d09bd3367da9a0dc183
Pavel Manylov <rapkasta@gmail.com>
2014-01-18 20:02:48 +0600

    SVG.Doc _defs.parent linked to Doc & related specs
    Without linking parent, we can not do complex things like:
    * creating mask on the mask (draw.mask().mask())
    * drawing on container like on the main canvas with access to global
    defs (draw.group().mask())

--- spec/spec/doc.js
+++ spec/spec/doc.js
@@ -3,9 +3,19 @@ describe('Doc', function() {
   it('should be an instance of SVG.Container', function() {
     expect(draw instanceof SVG.Container).toBe(true)
   })
-  
+
   it('should have a defs element', function() {
-    expect(draw instanceof SVG.Container).toBe(true)
+    expect(draw._defs instanceof SVG.Defs).toBe(true)
+  });
+
+  it('defs element parent should be this SVG.Doc', function(){
+    expect(draw._defs.parent).toBe(draw)
   })
-  
+
+  describe('defs()', function(){
+    it('should return defs element', function(){
+      expect(draw.defs()).toBe(draw._defs);
+    })
+  })
+
 })
