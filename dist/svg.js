0bec3f4b0eedfa603ed0b92e151315dfc92b42c2
wout <wout@impinc.co.uk>
2014-01-22 14:53:01 +0100

    Fix in viewbox

--- dist/svg.js
+++ dist/svg.js
@@ -1,4 +1,4 @@
-/* svg.js v0.33-5-gf8236f9 - svg regex default color array number viewbox bbox rbox element parent container fx event defs group arrange mask clip gradient doc shape use rect ellipse line poly path image text textpath nested sugar set memory loader - svgjs.com/license */
+/* svg.js v0.33-6-gc967856 - svg regex default color array number viewbox bbox rbox element parent container fx event defs group arrange mask clip gradient doc shape use rect ellipse line poly path image text textpath nested sugar set memory loader - svgjs.com/license */
 ;(function() {
 
   this.SVG = function(element) {
@@ -529,14 +529,31 @@
 
   SVG.ViewBox = function(element) {
     var x, y, width, height
+      , wm   = 1 /* width multiplier */
+      , hm   = 1 /* height multiplier */
       , box  = element.bbox()
       , view = (element.attr('viewBox') || '').match(/-?[\d\.]+/g)
+  
+    /* get dimensions of current node */
+    width  = new SVG.Number(element.width())
+    height = new SVG.Number(element.height())
+  
+    /* find nearest non-percentual dimensions */
+    while (width.unit == '%') {
+      wm *= width.value
+      width = new SVG.Number(element instanceof SVG.Doc ? element.parent.offsetWidth : element.width())
+    }
+    while (height.unit == '%') {
+      hm *= height.value
+      height = new SVG.Number(element instanceof SVG.Doc ? element.parent.offsetHeight : element.height())
+    }
     
-    /* clone attributes */
+    /* ensure defaults */
     this.x      = box.x
     this.y      = box.y
-    this.width  = element.node.clientWidth  || element.attr('width')
-    this.height = element.node.clientHeight || element.attr('height')
+    this.width  = width  * wm
+    this.height = height * hm
+    this.zoom   = 1
     
     if (view) {
       /* get width and height from viewbox */
@@ -555,10 +572,8 @@
       this.y      = y
       this.width  = width
       this.height = height
+      
     }
-    
-    /* ensure a default zoom value */
-    this.zoom = this.zoom || 1
     
   }
   
