a625bbce4d9bb909ae3ecb1dfa0aebe39fbf5d63
unknown <sgp0555@SGPP3M0555.bosch-si.com>
2014-02-09 15:00:05 +0800

    fix use dimensions

--- src/use.js
+++ src/use.js
@@ -15,6 +15,35 @@ SVG.Use = SVG.invent({
       /* set lined element */
       return this.attr('href', '#' + element, SVG.xlink)
     }
+    , width: function(width) {
+        var w = (this.bbox().width * this.trans.scaleX)
+        if(width) {
+            this.scale(width / w)
+        }
+        return w
+      }
+    , height: function(height) {
+        var h = (this.bbox().height * this.trans.scaleY)
+        if(height) {
+            this.scale(height / h)
+        }
+        return h
+      }
+    , x: function(x) {
+        if (x) {
+          x = new SVG.Number(x)
+          x.value = x.value / this.trans.scaleX - this.bbox().x
+        }
+        return this.attr('x', x)
+      }
+    , y: function(y) {
+        if (y) {
+          y = new SVG.Number(y)
+          y.value = y.value / this.trans.scaleY - this.bbox().y
+        }
+        return this.attr('y', y)
+      }
+    }
   }
   
   // Add parent method
