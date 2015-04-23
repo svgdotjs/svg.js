32e594e03a48b6b189cbef5607e2d4c8426f3023
Benjamin Brown <ben.brown@factmint.com>
2015-04-23 10:58:38 +0100

    Fix function declared inside if block that is not es5 strict mode compliant

--- src/polyfill.js
+++ src/polyfill.js
@@ -1,12 +1,12 @@
-// Add CustomEvent to IE9 and IE10 
+// Add CustomEvent to IE9 and IE10
 if (typeof CustomEvent !== 'function') {
   // Code from: https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent
-  function CustomEvent (event, options) {
+  var CustomEvent = function(event, options) {
     options = options || { bubbles: false, cancelable: false, detail: undefined }
     var e = document.createEvent('CustomEvent')
     e.initCustomEvent(event, options.bubbles, options.cancelable, options.detail)
     return e
-  }
+  };
 
   CustomEvent.prototype = window.Event.prototype
 
@@ -17,26 +17,26 @@ if (typeof CustomEvent !== 'function') {
 (function(w) {
   var lastTime = 0
   var vendors = ['moz', 'webkit']
-  
+
   for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
     w.requestAnimationFrame = w[vendors[x] + 'RequestAnimationFrame']
     w.cancelAnimationFrame  = w[vendors[x] + 'CancelAnimationFrame'] ||
                               w[vendors[x] + 'CancelRequestAnimationFrame']
   }
- 
-  w.requestAnimationFrame = w.requestAnimationFrame || 
+
+  w.requestAnimationFrame = w.requestAnimationFrame ||
     function(callback) {
       var currTime = new Date().getTime()
       var timeToCall = Math.max(0, 16 - (currTime - lastTime))
-      
+
       var id = w.setTimeout(function() {
         callback(currTime + timeToCall)
       }, timeToCall)
-      
+
       lastTime = currTime + timeToCall
       return id
     }
- 
+
   w.cancelAnimationFrame = w.cancelAnimationFrame || w.clearTimeout;
 
 }(window))
