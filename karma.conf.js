97cd2b7480b1d318dfab986c2ab8b058beb8d599
dotnetCarpenter <jon.ronnenberg@gmail.com>
2016-10-04 15:33:28 +0200

    trying out karma-fixture

--- karma.conf.js
+++ karma.conf.js
@@ -10,11 +10,12 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+    frameworks: ['jasmine', 'fixture'],
 
 
     // list of files / patterns to load in the browser
     files: [
+      'spec/fixture.svg',
       'dist/svg.js',
       'spec/**/*.js'
     ],
@@ -29,6 +30,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      '**/*.html': ['html2js']
     },
 
 
