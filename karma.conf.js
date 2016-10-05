ce4b12beea7124a40424237e8abf33d4a0840f47
dotnetCarpenter <jon.ronnenberg@gmail.com>
2016-10-05 13:52:33 +0200

    Many of the failing tests are rounding errors. Different browsers delivered different results. Thats why we took chrome as base.

--- karma.conf.js
+++ karma.conf.js
@@ -65,7 +65,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Firefox'],
+    browsers: ['Chromium'],
 
 
     // Continuous Integration mode
