3ced2c23ad464472010d6b700aef4aef57682268
dotnetCarpenter <jon.ronnenberg@gmail.com>
2017-04-27 14:08:00 +0200

    try to get better connection to soucelabs from travis and better log reports

--- .config/karma.conf.soucelabs.js
+++ .config/karma.conf.soucelabs.js
@@ -64,7 +64,7 @@ module.exports = function(config) {
       // web server port
       port: 9876,
 
-      reporters: ['progress', 'saucelabs'],
+      reporters: ['dots', 'saucelabs'],
       customLaunchers: SouceLabsLaunchers,
 
       // start these browsers
