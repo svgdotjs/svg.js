6614ff63842f80824ce23d154e61542f0f8ad48b
dotnetCarpenter <jon.ronnenberg@gmail.com>
2017-04-27 18:32:06 +0200

    made sure the test-runner runs with --single-run - now using spec reporter

--- .config/karma.conf.js
+++ .config/karma.conf.js
@@ -24,7 +24,7 @@ module.exports = function(config) {
       // test results reporter to use
       // possible values: 'dots', 'progress'
       // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-      reporters: ['dots', 'coverage'],
+      reporters: ['spec', 'coverage'],
 
       specReporter: {
         maxLogLines: 5,             // limit number of lines logged per test
@@ -60,7 +60,7 @@ module.exports = function(config) {
 
       // level of logging
       // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-      logLevel: config.LOG_DEBUG,
+      logLevel: config.LOG_INFO,
 
     })
 
--- .config/karma.conf.saucelabs.js
+++ .config/karma.conf.saucelabs.js
@@ -82,7 +82,7 @@ module.exports = function(config) {
         maxLogLines: 5,             // limit number of lines logged per test
         suppressErrorSummary: false, // do not print error summary
         suppressFailed: false,      // do not print information about failed tests
-        suppressPassed: false,      // do not print information about passed tests
+        suppressPassed: true,      // do not print information about passed tests
         suppressSkipped: true,      // do not print information about skipped tests
         showSpecTiming: true,      // print the time elapsed for each spec
         failFast: false              // test would finish with error when a first fail occurs.
