3c6a948f19e5822d5bdfbccfcb6074944f57268d
dotnetCarpenter <jon.ronnenberg@gmail.com>
2017-04-27 15:50:10 +0200

    try to fix connection issue - first change log level to DEBUG

--- .config/karma.conf.soucelabs.js
+++ .config/karma.conf.soucelabs.js
@@ -81,7 +81,7 @@ module.exports = function(config) {
 
       // level of logging
       // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-      logLevel: config.LOG_WARN,
+      logLevel: config.LOG_DEBUG,
 
       // enable / disable watching file and executing tests whenever any file changes
       autoWatch: false,
