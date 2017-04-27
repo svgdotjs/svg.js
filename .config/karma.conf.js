9c40addc6028a01293948764776427123f9deaad
dotnetCarpenter <jon.ronnenberg@gmail.com>
2017-04-27 06:08:45 +0200

    test souce labs on travis

--- .config/karma.conf.js
+++ .config/karma.conf.js
@@ -1,102 +1,59 @@
 // Karma configuration
-// Generated on Tue Oct 04 2016 13:53:46 GMT+0200 (CEST)
+
+const karmaCommon = require('./karma.common')
 
 module.exports = function(config) {
-  config.set({
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '../',
+  config.set(
 
+    Object.assign(karmaCommon, {
 
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+      // web server port
+      port: 9877,
 
+      // start these browsers
+      // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+      browsers: ['Firefox'],
 
-    // list of files / patterns to load in the browser
-    files: [
-      '.config/pretest.js',
-      {
-        pattern: 'spec/fixtures/fixture.css',
-        included: false,
-        served: true
+      // preprocess matching files before serving them to the browser
+      // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+      preprocessors: {
+        'dist/svg.js': ['coverage']
       },
-      {
-        pattern: 'spec/fixtures/fixture.svg',
-        included: false,
-        served: true
+
+      // test results reporter to use
+      // possible values: 'dots', 'progress'
+      // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+      reporters: ['progress', 'coverage'],
+
+      // configure the coverage reporter
+      coverageReporter: {
+        // Specify a reporter type.
+        type: 'lcov',
+        dir: 'coverage/',
+        subdir: function(browser) {
+          // normalization process to keep a consistent browser name accross different OS
+          return browser.toLowerCase().split(/[ /-]/)[0]; // output the results into: './coverage/firefox/'
+        }
       },
-      {
-        pattern: 'spec/fixtures/pixel.png',
-        included: false,
-        served: true
-      },
-      'dist/svg.js',
-      'spec/spec/**/*.js'
-    ],
 
-    // requests (http or link src) will be redirect from fixtures to base/spec/fixtures by the Karma server
-    proxies: {
-      '/fixtures/': '/base/spec/fixtures/'
-    },
+      // Concurrency level
+      // how many browser should be started simultaneous
+      concurrency: Infinity,
 
+      // enable / disable watching file and executing tests whenever any file changes
+      autoWatch: false,
 
-    // list of files to exclude
-    exclude: [],
+      // Continuous Integration mode
+      // if true, Karma captures browsers, runs the tests and exits
+      singleRun: false,
 
+      // level of logging
+      // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+      logLevel: config.LOG_INFO,
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-      'dist/svg.js': ['coverage']
-    },
+    })
 
+  )
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'coverage'],
-
-
-    // configure the coverage reporter
-    coverageReporter: {
-      // Specify a reporter type.
-      type: 'lcov',
-      dir: 'coverage/',
-      subdir: function(browser) {
-        // normalization process to keep a consistent browser name accross different OS
-        return browser.toLowerCase().split(/[ /-]/)[0]; // output the results into: './coverage/firefox/'
-      }
-    },
-
-
-    // web server port
-    port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Firefox'],
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true,
-
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity
-  })
 }
---
+++ .config/karma.conf.soucelabs.js
@@ -0,0 +1,92 @@
+// Karma configuration
+
+// TODO: better soucelab/karma setup: https://github.com/mzabriskie/axios/blob/master/karma.conf.js
+
+const karmaCommon = require('./karma.common')
+
+const SouceLabsLaunchers = {
+  /** Real mobile devices are not available
+   *  Your account does not have access to Android devices.
+   *  Please contact sales@saucelabs.com to add this feature to your account.
+  sl_android_chrome: {
+    base: 'SauceLabs',
+    browserName: 'Android',
+    appiumVersion: '1.5.3',
+    deviceName: 'Samsung Galaxy S7 Device',
+    deviceOrientation: 'portrait',
+    browserName: 'Chrome',
+    platformVersion: '6.0',
+    platformName: 'Android'
+  }, */
+  sl_android: {
+    browserName: 'Android',
+    deviceName: 'Samsung Galaxy S4 Emulator',
+    deviceOrientation: 'portrait'
+  },
+  sl_firefox: {
+    base: 'SauceLabs',
+    browserName: 'firefox',
+    version: 'latest'
+  },
+  sl_chrome: {
+    base: 'SauceLabs',
+    browserName: 'chrome',
+    version: 'latest'
+  },
+  sl_windows_edge: {
+    base: 'SauceLabs',
+    browserName: 'MicrosoftEdge',
+    version: 'latest',
+    platform: 'Windows 10'
+  },
+  sl_macos_safari: {
+    base: 'SauceLabs',
+    browserName: 'safari',
+    platform: 'macOS 10.12',
+    version: '10.0'
+  }
+}
+
+module.exports = function(config) {
+
+  config.set(
+
+    Object.assign(karmaCommon, {
+
+      // web server port
+      port: 9876,
+
+      reporters: ['progress', 'saucelabs'],
+      customLaunchers: SouceLabsLaunchers,
+
+      // start these browsers
+      browsers: Object.keys(SouceLabsLaunchers),
+      sauceLabs: {
+          testName: 'SVG.js Unit Tests'
+      },
+      browserDisconnectTimeout: 60 * 1000,
+      browserDisconnectTolerance: 2,
+      browserNoActivityTimeout: 4 * 60 * 1000,
+
+      // enable / disable colors in the output (reporters and logs)
+      colors: true,
+
+      // level of logging
+      // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+      logLevel: config.LOG_DEBUG,
+
+      // enable / disable watching file and executing tests whenever any file changes
+      autoWatch: false,
+
+      // Continuous Integration mode
+      // if true, Karma captures browsers, runs the tests and exits
+      singleRun: true,
+
+      // Concurrency level
+      // how many browser should be started simultaneous
+      concurrency: 5
+    })
+
+  )
+
+}
