b913c7dd3af1921b7f343447ea884599cb4fb99d
Makarov Alexandr <next.meaning@gmail.com>
2017-11-19 19:56:50 +1000

    Fix typings for arrays of points implemented as array. (Ex.: [[0, 0], [100, 100]])

--- svg.js.d.ts
+++ svg.js.d.ts
@@ -423,7 +423,8 @@ declare namespace svgjs {
 
     // line.js
     interface ArrayPoint extends Array<number> { }
-    type PointArrayAlias = ArrayPoint | number[] | PointArray | string;
+    type ArrayAsPoint = [number, number];
+    type PointArrayAlias = ArrayPoint | number[] | ArrayAsPoint[] | PointArray | string;
     
     export interface Line extends Shape {
         new (): Line;
