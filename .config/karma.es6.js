54d407608efec1ca37f18178082bb9703cf8bc1e
Ulrich-Matthias Schäfer <ulima.ums@googlemail.com>
2018-11-13 10:10:32 +0100

    add config for karma to run es6 tests

---
+++ .config/karma.es6.js
@@ -0,0 +1,115 @@
+// Karma configuration
+// Generated on Tue Oct 04 2016 13:53:46 GMT+0200 (CEST)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '../',
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      'spec/RAFPlugin.js',
+      {
+        pattern: 'spec/fixtures/fixture.css',
+        included: false,
+        served: true
+      },
+      {
+        pattern: 'spec/fixtures/pixel.png',
+        included: false,
+        served: true
+      },
+      {
+        pattern: 'src/**/*.js',
+        included: false,
+        served: true,
+        type: 'modules'
+      },
+      {
+        pattern: 'spec/helpers.js',
+        included: false,
+        served: true,
+        type: 'module'
+      },
+      {
+        pattern: 'spec/setupBrowser.js',
+        included: true,
+        type: 'module'
+      },
+      {
+        pattern: 'spec/spec/types/*.js',
+        included: true,
+        type: 'module'
+      }
+    ],
+
+    proxies: {
+      '/fixtures/': '/base/spec/fixtures/'
+    },
+
+
+    // list of files to exclude
+    exclude: [],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+    //  'src/**/*.js': ['coverage']
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'/*, 'coverage'*/],
+
+
+    // configure the coverage reporter
+    coverageReporter: {
+      // Specify a reporter type.
+      type: 'lcov',
+      dir: 'coverage/',
+      subdir: function(browser) {
+        // normalization process to keep a consistent browser name accross different OS
+        return browser.toLowerCase().split(/[ /-]/)[0]; // output the results into: './coverage/firefox/'
+      }
+    },
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: [/*'ChromeHeadless', */'FirefoxHeadless'],
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity
+  })
+}
