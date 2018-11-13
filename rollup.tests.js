57d7dd0232b283874f09bb08e6d05ccfc6d1da24
Ulrich-Matthias Schäfer <ulima.ums@googlemail.com>
2018-11-13 11:19:05 +0100

    make es5 tests

---
+++ rollup.tests.js
@@ -0,0 +1,29 @@
+import babel from 'rollup-plugin-babel'
+import multiEntry from 'rollup-plugin-multi-entry'
+
+export default {
+  input: ['spec/setupBrowser.js', 'spec/spec/types/*.js'],
+  output: {
+    file: 'spec/es5TestBundle.js',
+    name: 'SVGTests',
+    sourceMap: true,
+    format: 'iife'
+  },
+  plugins: [
+    babel({
+      include: 'src/**',
+      runtimeHelpers: true,
+      babelrc: false,
+      presets: [["@babel/preset-env", {
+        modules: false,
+      }]],
+      // plugins: [["@babel/plugin-transform-runtime", {
+      //   corejs: false,
+      //   helpers: true,
+      //   regenerator: true,
+      //   useESModules: true
+      // }]]
+    }),
+    multiEntry()
+  ]
+}
