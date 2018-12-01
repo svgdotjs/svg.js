dbe9c376fd76dd4e4777281888f4092e38512c18
Ulrich-Matthias Schäfer <ulima.ums@googlemail.com>
2018-12-01 14:57:30 +0100

    add back move and dmove for groups

--- src/elements/G.js
+++ src/elements/G.js
@@ -6,6 +6,32 @@ export default class G extends Container {
   constructor (node) {
     super(nodeOrNew('g', node), node)
   }
+
+  x (x) {
+    if (x == null) return this.transform()['x']
+    return this.move(x, 0)
+  }
+
+  y (y) {
+    if (y == null) return this.transform()['y']
+    return this.move(0, y)
+  }
+
+  move (x, y) {
+    return this.translate(x, y)
+  }
+
+  dx (dx) {
+    return this.transform({ dx }, true)
+  }
+
+  dy (dy) {
+    return this.transform({ dy }, true)
+  }
+
+  dmove (dx, dy) {
+    return this.transform({ dx, dy }, true)
+  }
 }
 
 registerMethods({
