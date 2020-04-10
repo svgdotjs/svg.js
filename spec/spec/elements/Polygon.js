6ac53822882f5cc852c8e8e41e9507333fed2180
Ulrich-Matthias Schäfer <ulima.ums@googlemail.com>
2020-04-10 16:03:05 +1000

    added tests for Polygon and Polyline

---
+++ spec/spec/elements/Polygon.js
@@ -0,0 +1,35 @@
+/* globals describe, expect, it, jasmine */
+
+import { Polygon, G } from '../../../src/main.js'
+
+const { any } = jasmine
+
+describe('Polygon.js', () => {
+  describe('()', () => {
+    it('creates a new object of type Polygon', () => {
+      expect(new Polygon()).toEqual(any(Polygon))
+    })
+
+    it('sets passed attributes on the element', () => {
+      expect(new Polygon({ id: 'foo' }).id()).toBe('foo')
+    })
+  })
+
+  describe('Container', () => {
+    describe('polygon()', () => {
+      it('creates a polygon with given points', () => {
+        const group = new G()
+        const polygon = group.polygon([ 1, 2, 3, 4 ])
+        expect(polygon.array()).toEqual([ [ 1, 2 ], [ 3, 4 ] ])
+        expect(polygon).toEqual(any(Polygon))
+      })
+    })
+
+    it('creates a polygon with one point by default', () => {
+      const group = new G()
+      const polygon = group.polygon()
+      expect(polygon.array()).toEqual([ [ 0, 0 ] ])
+      expect(polygon).toEqual(any(Polygon))
+    })
+  })
+})
---
+++ spec/spec/elements/Polyline.js
@@ -0,0 +1,35 @@
+/* globals describe, expect, it, jasmine */
+
+import { Polyline, G } from '../../../src/main.js'
+
+const { any } = jasmine
+
+describe('Polyline.js', () => {
+  describe('()', () => {
+    it('creates a new object of type Polyline', () => {
+      expect(new Polyline()).toEqual(any(Polyline))
+    })
+
+    it('sets passed attributes on the element', () => {
+      expect(new Polyline({ id: 'foo' }).id()).toBe('foo')
+    })
+  })
+
+  describe('Container', () => {
+    describe('polyline()', () => {
+      it('creates a polyline with given points', () => {
+        const group = new G()
+        const polyline = group.polyline([ 1, 2, 3, 4 ])
+        expect(polyline.array()).toEqual([ [ 1, 2 ], [ 3, 4 ] ])
+        expect(polyline).toEqual(any(Polyline))
+      })
+
+      it('creates a polyline with one point by default', () => {
+        const group = new G()
+        const polyline = group.polyline()
+        expect(polyline.array()).toEqual([ [ 0, 0 ] ])
+        expect(polyline).toEqual(any(Polyline))
+      })
+    })
+  })
+})
